b1cb85e074f6583b88d3a13cd13e738f4e794b26
Dan Lee <71398022+dandhlee@users.noreply.github.com>
2021-06-01 14:55:51 -0400

    feat: Update shell script

--- .kokoro/generate-docs.sh
+++ .kokoro/generate-docs.sh
@@ -24,84 +24,75 @@ export PATH="${HOME}/.local/bin:${PATH}"
 python3 -m pip install --user --quiet --upgrade nox
 python3 -m pip install --user gcp-docuploader
 
-# Retrieve repositories to regenerate the YAML with.
-for bucket_item in $(gsutil ls 'gs://docs-staging-v2/docfx-python*'); do
+# Retrieve unique repositories to regenerate the YAML with.
+for bucket_item in $(gsutil ls 'gs://docs-staging-v2/docfx-python*' | sort -u -t- -k5,5); do
 
-  # Parse the library name.
-  library=$(echo ${bucket_item} | cut -d "-" -f 5)
+  # Retrieve the GitHub Repository info.
+  gsutil cp ${bucket_item} .
+  tarball=$(echo ${bucket_item} | cut -d "/" -f 4)
+  tar -xf ${tarball} docs.metadata
+  repo=$(cat docs.metadata | grep "github_repository:" | cut -d "\"" -f 2 | cut -d "/" -f 2)
 
-  # Only process unique entries.
-  if [[ ${prev} != ${library} ]]; then
-    prev=${library}
+  echo "cloning ${repo}..."
+  git clone https://github.com/googleapis/${repo}.git
 
-    # Retrieve the GitHub Repository info.
-    gsutil cp ${bucket_item} .
-    tarball=$(echo ${bucket_item} | cut -d "/" -f 4)
-    tar -xf ${tarball} docs.metadata
-    repo=$(cat docs.metadata | grep "github_repository:" | cut -d "\"" -f 2 | cut -d "/" -f 2)
+  # Clean up resources we don't need anymore.
+  rm ${tarball}
+  rm docs.metadata
 
-    echo "cloning ${repo}..."
-    git clone https://github.com/googleapis/${repo}.git
+  # For each repo, process docs and docfx jobs to regenerate the YAML.
+  cd ${repo}
 
-    # Clean up resources we don't need anymore.
-    rm ${tarball}
-    rm docs.metadata
+  if [ ${FORCE_GENERATE_ALL_TAGS} = "true" ]; then
+    # Grabs all tags from the repository
+    GITHUB_TAGS=$(git tag --sort=-v:refname)
+  else
+    # Grab the latest released tag
+    GITHUB_TAGS=$(git describe --tags `git rev-list --tags --max-count=1`)
+  fi
 
-    # For each repo, process docs and docfx jobs to regenerate the YAML.
-    cd ${repo}
+  # TODO: allow skipping failing docs builds and continue with the rest of the generation.
+  for tag in ${GITHUB_TAGS}; do
+    git checkout ${tag}
 
-    if [ ${FORCE_GENERATE_ALL_TAGS} = "true" ]; then
-      # Grabs all tags from the repository
-      GITHUB_TAGS=$(git tag --sort=-v:refname)
-    else
-      # Grab the latest released tag
-      GITHUB_TAGS=$(git describe --tags `git rev-list --tags --max-count=1`)
-    fi
+    # TODO: support building all googleapis.dev docs through an environmental variable option passed.
+    ## Build HTML docs for googleapis.dev.
+    # nox -s docs
 
-    # TODO: allow skipping failing docs builds and continue with the rest of the generation.
-    for tag in ${GITHUB_TAGS}; do
-      git checkout ${tag}
+    # python3 -m docuploader create-metadata \
+    #  --name=$(jq --raw-output '.name // empty' .repo-metadata.json) \
+    #  --version=$(python3 setup.py --version) \
+    #  --language=$(jq --raw-output '.language // empty' .repo-metadata.json) \
+    #  --distribution-name=$(python3 setup.py --name) \
+    #  --product-page=$(jq --raw-output '.product_documentation // empty' .repo-metadata.json) \
+    #  --github-repository=$(jq --raw-output '.repo // empty' .repo-metadata.json) \
+    #  --issue-tracker=$(jq --raw-output '.issue_tracker // empty' .repo-metadata.json)
 
-      # TODO: support building all googleapis.dev docs through an environmental variable option passed.
-      ## Build HTML docs for googleapis.dev.
-      # nox -s docs
+    # cat docs.metadata
 
-      # python3 -m docuploader create-metadata \
-      #  --name=$(jq --raw-output '.name // empty' .repo-metadata.json) \
-      #  --version=$(python3 setup.py --version) \
-      #  --language=$(jq --raw-output '.language // empty' .repo-metadata.json) \
-      #  --distribution-name=$(python3 setup.py --name) \
-      #  --product-page=$(jq --raw-output '.product_documentation // empty' .repo-metadata.json) \
-      #  --github-repository=$(jq --raw-output '.repo // empty' .repo-metadata.json) \
-      #  --issue-tracker=$(jq --raw-output '.issue_tracker // empty' .repo-metadata.json)
-
-      # cat docs.metadata
-
-      ## upload docs
-      # python3 -m docuploader upload docs/_build/html --metadata-file docs.metadata --staging-bucket "${STAGING_BUCKET}"
+    ## upload docs
+    # python3 -m docuploader upload docs/_build/html --metadata-file docs.metadata --staging-bucket "${STAGING_BUCKET}"
 
 
-      # Build YAML tarballs for Cloud-RAD.
-      nox -s docfx
+    # Build YAML tarballs for Cloud-RAD.
+    nox -s docfx
 
-      python3 -m docuploader create-metadata \
-        --name=$(jq --raw-output '.name // empty' .repo-metadata.json) \
-        --version=$(python3 setup.py --version) \
-        --language=$(jq --raw-output '.language // empty' .repo-metadata.json) \
-        --distribution-name=$(python3 setup.py --name) \
-        --product-page=$(jq --raw-output '.product_documentation // empty' .repo-metadata.json) \
-        --github-repository=$(jq --raw-output '.repo // empty' .repo-metadata.json) \
-        --issue-tracker=$(jq --raw-output '.issue_tracker // empty' .repo-metadata.json)
+    python3 -m docuploader create-metadata \
+      --name=$(jq --raw-output '.name // empty' .repo-metadata.json) \
+      --version=$(python3 setup.py --version) \
+      --language=$(jq --raw-output '.language // empty' .repo-metadata.json) \
+      --distribution-name=$(python3 setup.py --name) \
+      --product-page=$(jq --raw-output '.product_documentation // empty' .repo-metadata.json) \
+      --github-repository=$(jq --raw-output '.repo // empty' .repo-metadata.json) \
+      --issue-tracker=$(jq --raw-output '.issue_tracker // empty' .repo-metadata.json)
 
-      cat docs.metadata
+    cat docs.metadata
 
-      # upload docs
-      python3 -m docuploader upload docs/_build/html/docfx_yaml --metadata-file docs.metadata --destination-prefix docfx --staging-bucket "${V2_STAGING_BUCKET}"
-
-    done
-
-    # Clean up the repository to make room.
-    cd ../
-    rm -rf ${repo}
-  fi 
+    # upload docs
+    python3 -m docuploader upload docs/_build/html/docfx_yaml --metadata-file docs.metadata --destination-prefix docfx --staging-bucket "${V2_STAGING_BUCKET}"
+  done
+  
+  # Clean up the repository to make room.
+  cd ../
+  rm -rf ${repo} 
 done
